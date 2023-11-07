3b77d6eb98d8109bc292acf4849ed2c105b22b48
MilagrosFrontend <milagrosfrontend@gmail.com>
2023-11-06 21:06:53 -0300

    solucionado el bug del modal en el home

--- js/index.js
+++ js/index.js
@@ -48,7 +48,7 @@ window.addEventListener('load', function() {
   
       });
     });
-    },300)  // Upcoming es una consulta que requiere más tiempo
+    },400)  // Upcoming es una consulta que requiere más tiempo
 
 })
 
